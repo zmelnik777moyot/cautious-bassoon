28f3ade47a6ce2022836e9e9b5b4d6f034b9ca59
zmelnik777moyot <zmelnik2020@mail.ru>
2025-04-25 18:40:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "8089851764:AAEbJmSeFb8lY1LbHJLqMy9c6ZVpY8gWw_g"
-WEBHOOK_URL = "https://api.render.com/deploy/srv-d04hma24d50c73a6uvk0?key=zkGvdizp7Lk"
+WEBHOOK_URL = "https://zmelnik777moyot.github.io/cautious-bassoon/"
 ADMINS = [123456789]
