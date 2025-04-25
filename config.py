2bd704b657621c2e128e9ba2457e18484e7f93d6
zmelnik777moyot <zmelnik2020@mail.ru>
2025-04-25 18:36:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "ВАШ_ТОКЕН_БОТА"
-WEBHOOK_URL = "ВАШ_URL_НА_RENDER/webhook"
+BOT_TOKEN = "8089851764:AAEbJmSeFb8lY1LbHJLqMy9c6ZVpY8gWw_g"
+WEBHOOK_URL = "https://api.render.com/deploy/srv-d04hma24d50c73a6uvk0?key=zkGvdizp7Lk"
 ADMINS = [123456789]
