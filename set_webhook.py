8f183a348779275d8e0a162369a641ccaa23feef
zmelnik777moyot <zmelnik2020@mail.ru>
2025-04-29 00:40:49 +0300

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -2,7 +2,7 @@ import requests
 
 # Вставь сюда свой токен бота и адрес сайта:
 BOT_TOKEN = "8089851764:AAEbJmSeFb8lY1LbHJLqMy9c6ZVpY8gWw_g"
-WEBHOOK_URL = "cautious-bassoon-production.up.railway.app"
+WEBHOOK_URL = "cautious-bassoon-production.up.railway.app/webhook"
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook"
 payload = {"url": WEBHOOK_URL}
