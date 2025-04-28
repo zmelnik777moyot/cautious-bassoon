da6fe054cb917359ffcd26c6d4b686cbda63cfae
zmelnik777moyot <zmelnik2020@mail.ru>
2025-04-29 00:23:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "8089851764:AAEbJmSeFb8lY1LbHJLqMy9c6ZVpY8gWw_g"
-WEBHOOK_URL = "https://zmelnik777moyot.github.io/cautious-bassoon/"
+WEBHOOK_URL = "cautious-bassoon.railway.internal"
 ADMINS = [123456789]
